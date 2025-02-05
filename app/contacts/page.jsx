2217a6f00d25c636b49db6785b3730cea44ecf9a
Max Mendelev <mmendelev@navirit.by>
2025-02-05 17:37:56 +0300

    Update project

--- app/contacts/page.jsx
+++ app/contacts/page.jsx
@@ -1,13 +1,11 @@
 import ContactsBlock from '@/components/Contacts/ContactsBlock/ContactsBlock'
 import Details from '@/components/Contacts/Details/Details'
 import MapComponent from '@/components/Contacts/MapComponent/MapComponent'
-import YandexMap from '@/components/Contacts/YandexMap/YandexMap'
 import ContactUs from '@/components/UI/ContactUs/ContactUs'
 
 export default function page() {
 	return (
 		<div>
-			<YandexMap />
 			<ContactsBlock />
 			<MapComponent />
 			<Details />
