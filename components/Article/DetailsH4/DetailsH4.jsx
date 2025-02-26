6115c26887b1f147ad9e65e61d76ff21f91371d7
Max Mendelev <mmendelev@navirit.by>
2025-02-26 13:05:01 +0300

    Update project

--- components/Article/DetailsH4/DetailsH4.jsx
+++ components/Article/DetailsH4/DetailsH4.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import styles from "./DetailsH4.module.scss";
+import {isValidSubData} from "@/utils/validation";
 
 export default function DetailsH4({ details }) {
   return (
     <div className={styles.h4Container}>
       <h4>{details.title}</h4>
         <div className={styles.h4Content}>
-          { details?.points && details?.points?.length > 0 && (
+          { details?.points && isValidSubData(details.points) && (
             <ul>
-              {details.points?.map((point, index) => (
-                <li key={index}>{point}</li>
-              ))}
+              {Array.isArray(details.points) ? (
+                <>
+                    { details?.points?.length > 0 && details.points.map((point, index) => (
+                        <li key={index}>{point}</li>
+                    ))}
+                </>
+              ) : (
+                  <li>{details.points}</li>
+              )}
             </ul>
           )}
           { details?.conclusion && (<div className={styles.conclusion}>{details.conclusion}</div>)}
