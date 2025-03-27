9ea7a52abd9ee97027bd1ffe4a500966147e519f
Max Mendelev <mmendelev@navirit.by>
2025-03-27 09:41:02 +0300

    Update project

--- app/promyshlennye-roboty/[id]/[slug]/page.jsx
+++ app/promyshlennye-roboty/[id]/[slug]/page.jsx
@@ -1,36 +1,63 @@
-'use client';
+import { notFound } from "next/navigation";
+import ProductPageClient from "./ProductPageClient";
 
-import { useParams } from "next/navigation";
-import Products from "@/components/Products/Products";
-import Breadcrumbs from "@/components/UI/Breadcrumbs/Breadcrumbs";
-import useProducts from "@/hooks/useProducts";
-import useCategories from "@/hooks/useCategories";
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL_API;
+const API_AUTH = process.env.NEXT_PUBLIC_API_AUTH;
 
-export default function Page() {
-  const { products } = useProducts();
-  const { categories } = useCategories(true);
-  const { id,  slug } = useParams(); 
+async function fetchData(endpoint, method = "GET") {
+  const response = await fetch(`${BASE_URL}/${endpoint}`, {
+    method,
+    headers: {
+      Auth: API_AUTH,
+      "Content-Type": "application/json",
+    },
+  });
 
- 
-  const productData = products?.find((product) => product.slug === slug);
-  const productName = productData?.title;
-  const currentCategory = categories.find(
-    (category) => category?.key?.toString() === productData?.category?.toString()
-  );
-  const parentCategory = categories.find(
-    (category) => category?.key?.toString() === currentCategory?.parent?.toString()
-  );
-  const breadcrumbItems = [
-    { label: "Главная", link: "/" },
-    { label: "Промышленные роботы", link: "/promyshlennye-roboty" },
-    { label: currentCategory?.name, link: currentCategory?.link },
-    { label: productName, link: "" },
-  ];
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.message || "Error fetching data");
+  }
+  return response.json();
+}
+
+async function getProductBySlug(slug) {
+  const data = await fetchData("product", "GET");
+  const products = data?.data || [];
+  return products.find((product) => product.slug === slug);
+}
+
+
+export async function generateMetadata({ params }) {
+  const { slug } = params;
+  let productData;
+  try {
+    productData = await getProductBySlug(slug);
+  } catch (error) {
+    console.error("Ошибка получения данных продукта для метаданных:", error);
+  }
+
+  if (!productData) {
+    return {
+      title: "Продукт не найден",
+      description: "",
+    };
+  }
+
+  return {
+    title: productData.meta_title || productData.title,
+    description: productData.meta_description || "",
+    keywords: productData.meta_keywords,
+  };
+}
+
+export default async function Page({ params }) {
+  const { slug } = params;
+  const productData = await getProductBySlug(slug);
+  if (!productData) notFound();
 
   return (
-    <div>
-      <Breadcrumbs items={breadcrumbItems} />
-      <Products productData={productData} parentCategory={parentCategory} />
-    </div>
+    <ProductPageClient
+    productData={productData}
+  />
   );
 }
